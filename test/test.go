ec9a099a20ad6a9f077dc8bc201bea2b1194667c
Michal Klimuk <michal@gophertribe.com>
2025-12-17 14:15:26 +0100

    chore: bump up to golangci-lint v2

--- test/test.go
+++ test/test.go
@@ -6,19 +6,25 @@ import (
 
 // Test runs unit tests
 func Test() error {
-	return sh.RunV("go", "run", "gotest.tools/gotestsum@v1.12.0", "--no-summary=skipped", "--junitfile", "./coverage.xml", "--format", "short", "./...")
+	env := map[string]string{
+		"GOPRIVATE": "github.com/gophertribe,github.com/mklimuk,github.com/satsysoft",
+	}
+	return sh.RunWithV(env, "go", "run", "gotest.tools/gotestsum@v1.13.0", "--no-summary=skipped", "--junitfile", "./coverage.xml", "--format", "short", "./...")
 }
 
 // Lint runs the linter
 func Lint() error {
-	return sh.RunV("go", "run", "github.com/golangci/golangci-lint/cmd/golangci-lint@v1.62.0", "run", "--timeout", "5m", "--skip-dirs", "dist", "./...")
+	env := map[string]string{
+		"GOPRIVATE": "github.com/gophertribe,github.com/mklimuk,github.com/satsysoft",
+	}
+	return sh.RunWithV(env, "go", "run", "github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.7.2", "run", "--timeout", "5m", "./...")
 }
 
 // Integ runs integration tests
 func Integ() error {
 	env := map[string]string{
-		"GOPRIVATE":                "github.com/gophertribe,github.com/mklimuk",
+		"GOPRIVATE":                "github.com/gophertribe,github.com/mklimuk,github.com/satsysoft",
 		"TEST_INTEGRATION_ENABLED": "1",
 	}
-	return sh.RunWithV(env, "go", "run", "gotest.tools/gotestsum@v1.12.0", "--no-summary=skipped", "--junitfile", "./coverage.xml", "--format", "short", "./...")
+	return sh.RunWithV(env, "go", "run", "gotest.tools/gotestsum@v1.13.0", "--no-summary=skipped", "--junitfile", "./coverage.xml", "--format", "short", "./...")
 }
